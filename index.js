09b71794a7d31ee4feae8f1e69954a1bde7a3bb9
Barbara Tanaka Menezes <128565098+barbtm@users.noreply.github.com>
2023-11-24 07:30:00 -0300

    Atualizar o index.js do clone

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-const form = document.querySelector("#form");
-const emailInput = document.querySelector("#email");
+const formInput = document.querySelector("form");
+const emailInput = document.querySelector("email");
 const passwordInput = document.getElementById("password"); 
+const buttonInput = document.getElementeById ("myButton")
 
-form.addEventListener ("btn", (event) => {
+form.addEventListener ( function (event = click) => {
     event.preventDefault();
 
     //verifica se o email esta preenchido e se é valido
-    if(emailInput.value === "" || !isEmailValid(emailInput.value)){
+    if(email.value === "" || !isEmailValid(email.value)){
         alert("Insira endereço de e-mail");
         return;
     }
-
-    //verifica se a senha esta preenchida
-    if (!validatePassword((passwordInput.value))) {
-        alert("A senha precisa ter no mínimo 8 dígitos.");
-        return;
-}
-
-    //Enviar se tudo estiver preenchido
-    form.getElementsByTagName("btn")[0].click();
-})
-
+    
 //função de validação de e-mail
 function isEmailValid(email){
     //regex de validação
@@ -35,6 +26,12 @@ function isEmailValid(email){
     return false;
 }   
 
+
+//verifica se a senha esta preenchida
+    if (!validatePassword((password.value))) {
+        alert("A senha precisa ter no mínimo 8 dígitos.");
+        return;
+}
 //funcção de validação de senha
 function validatePassword(password, minDigits=8) {
     if (password.length >= minDigits ) {
@@ -43,6 +40,37 @@ function validatePassword(password, minDigits=8) {
     }
     // senha inválida
     return false;
+}
+
+
+    function 
+
+    //Enviar se tudo estiver preenchido
+    form.getElementsByTagName("myButton");
+})
+
+//função de validação de e-mail
+//function isEmailValid(email){
+    //regex de validação
+    //const emailRegex = new RegExp(
+        //usuario12@host.com.br
+        //^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    );
+
+    //if(emailRegex.test(email)){
+        //return true;
+    }
+    //return false;
+}   
+
+//funcção de validação de senha
+//function validatePassword(password, minDigits=8) {
+    //if (password.length >= minDigits ) {
+      // senha válida
+    //return true;
+    }
+    // senha inválida
+    //return false;
 }
 
 
